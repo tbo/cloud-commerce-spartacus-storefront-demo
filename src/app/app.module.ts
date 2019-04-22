5ac31ed7918f36c7a8406847838cc0709b6dfa74
Thomas Bonk <tbo@cybo.biz>
2019-04-22 13:27:01 +0200

    Added translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { StorefrontModule } from "@spartacus/storefront";
+import { translations } from "@spartacus/storefront";
 
 const config = {
   backend: {
@@ -14,6 +15,9 @@ const config = {
   },
   site: {
     baseSite: "electronics"
+  },
+  i18n: {
+    resources: translations
   }
 };
 
