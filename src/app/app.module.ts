f90a96954d6be668e8d5fa703ce8e6a71ca637e1
Thomas Bonk <tbo@cybo.biz>
2019-04-22 13:06:35 +0200

    Added demo config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,25 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { StorefrontModule } from "@spartacus/storefront";
 
+const config = {
+  backend: {
+    occ: {
+      baseUrl:
+        "https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com"
+    }
+  },
+  site: {
+    baseSite: "electronics"
+  }
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StorefrontModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StorefrontModule.withConfig(config)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
